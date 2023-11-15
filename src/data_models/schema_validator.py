fc989a0873f4bc123c8fd1b8d10549d755a14fb6
Abu Desai <abhyuday.desai@gmail.com>
2023-11-15 14:56:33 -0800

    update schema validator for forecasting schema

--- src/data_models/schema_validator.py
+++ src/data_models/schema_validator.py
@@ -1,6 +1,6 @@
 from collections import Counter
 from enum import Enum
-from typing import List, Optional
+from typing import List, Optional, Union
 
 from pydantic import BaseModel, ValidationError, validator
 
@@ -9,48 +9,58 @@ class ID(BaseModel):
     """
     A model representing the ID field of the dataset.
     """
-
     name: str
     description: str
 
 
-class Target(BaseModel):
+class ForecastTarget(BaseModel):
     """
-    A model representing the target field of a binary classification problem.
+    A model representing the target field of forecasting problem.
     """
-
     name: str
     description: str
-    classes: List[str]
-
-    @validator("classes", allow_reuse=True)
-    def target_classes_are_two_and_unique_and_not_empty_str(cls, target_classes):
-        if len(target_classes) != 2:
-            raise ValueError(
-                f"Target classes must be a list with two labels."
-                f"Given `{target_classes}`"
-            )
-        if len(set(target_classes)) != 2:
-            raise ValueError(
-                "Target classes must be a list with two unique labels. "
-                f"Given `{target_classes}`"
-            )
-        if "" in target_classes:
-            raise ValueError(
-                "Target classes must not contain empty strings. "
-                f"Given `{target_classes}`"
-            )
-        return target_classes
+    dataType: str
+    example: float
 
 
-class DataType(str, Enum):
+class TimeDataType(str, Enum):
     """Enum for the data type of a feature"""
 
+    DATE = "DATE"
+    DATETIME = "DATETIME"
+    INT = "INT"
+
+
+class TimeField(BaseModel):
+    """
+    A model representing the target field of forecasting problem.
+    """
+    name: str
+    description: str
+    dataType: TimeDataType
+    example: Union[float, str]
+
+
+class CovariateDataType(str, Enum):
+    """Enum for the data type of a Covariate"""
+
     NUMERIC = "NUMERIC"
-    CATEGORICAL = "CATEGORICAL"
+    INT = "INT"
+    FLOAT = "FLOAT"
 
 
-class Feature(BaseModel):
+class Frequency(str, Enum):
+    """Enum for the frequency of the time series"""
+
+    HOURLY = "HOURLY"
+    DAILY = "DAILY"
+    WEEKLY = "WEEKLY"
+    MONTHLY = "MONTHLY"
+    QUARTERLY = "QUARTERLY"
+    YEARLY = "YEARLY"
+    OTHER = "OTHER"
+
+class Covariate(BaseModel):
     """
     A model representing the predictor fields in the dataset. Validates the
     presence and type of the 'example' field based on the 'dataType' field
@@ -60,46 +70,8 @@ class Feature(BaseModel):
 
     name: str
     description: str
-    dataType: DataType
-    nullable: bool
+    dataType: CovariateDataType
     example: Optional[float]
-    categories: Optional[List[str]]
-
-    @validator("example", always=True, allow_reuse=True)
-    def example_is_present_with_data_type_is_numeric(cls, v, values):
-        data_type = values.get("dataType")
-        if data_type == "NUMERIC" and v is None:
-            raise ValueError(
-                f"`example` must be present and a float or an integer "
-                f"when dataType is NUMERIC. Check field: {values}"
-            )
-        return v
-
-    @validator("categories", always=True, allow_reuse=True)
-    def categories_are_present_with_data_type_is_categorical(cls, v, values):
-        data_type = values.get("dataType")
-        if data_type == "CATEGORICAL" and v is None:
-            raise ValueError(
-                "`categories` must be present when dataType is CATEGORICAL. "
-                f"Check field: {values}"
-            )
-        return v
-
-    @validator("categories", always=True, allow_reuse=True)
-    def categories_are_non_empty_strings(cls, v, values):
-        categories = values.get("categories")
-        if categories is not None:
-            if len(categories) == 0:
-                raise ValueError(
-                    f"`categories` must not be empty. Check field: {values}"
-                )
-            for category in categories:
-                if str(category) == "" or not isinstance(category, str):
-                    raise ValueError(
-                        f"`categories` must be a list of strings. Check field: {values}"
-                    )
-        return v
-
 
 class SchemaModel(BaseModel):
     """
@@ -113,15 +85,18 @@ class SchemaModel(BaseModel):
     schemaVersion: float
     inputDataFormat: str = None
     encoding: str = None
-    id: ID
-    target: Target
-    features: List[Feature]
+    frequency: Frequency
+    idField: ID
+    timeField: TimeField
+    forecastTarget: ForecastTarget
+    pastCovariates: List[Covariate]
+    futureCovariates: List[Covariate]
 
     @validator("modelCategory", allow_reuse=True)
     def valid_problem_category(cls, v):
-        if v != "binary_classification":
+        if v != "forecasting":
             raise ValueError(
-                f"modelCategory must be 'binary_classification'. Given {v}"
+                f"modelCategory must be 'forecasting'. Given {v}"
             )
         return v
 
@@ -131,18 +106,10 @@ class SchemaModel(BaseModel):
             raise ValueError(f"schemaVersion must be set to 1.0. Given {v}")
         return v
 
-    @validator("features", allow_reuse=True)
-    def at_least_one_predictor_field(cls, v):
-        if len(v) < 1:
-            raise ValueError(
-                f"features must have at least one field defined. Given {v}"
-            )
-        return v
-
-    @validator("features", allow_reuse=True)
-    def unique_feature_names(cls, v):
+    @validator("pastCovariates", allow_reuse=True)
+    def unique_past_covariate_names(cls, v):
         """
-        Check that the feature names are unique.
+        Check that the past covariates names are unique.
         """
         feature_names = [feature.name for feature in v]
         duplicates = [
@@ -151,7 +118,24 @@ class SchemaModel(BaseModel):
 
         if duplicates:
             raise ValueError(
-                "Duplicate feature names found in schema: " f"`{', '.join(duplicates)}`"
+                "Duplicate past covariates names found in schema: " f"`{', '.join(duplicates)}`"
+            )
+
+        return v
+
+    @validator("futureCovariates", allow_reuse=True)
+    def unique_future_covariate_names(cls, v):
+        """
+        Check that the future covariates names are unique.
+        """
+        feature_names = [feature.name for feature in v]
+        duplicates = [
+            item for item, count in Counter(feature_names).items() if count > 1
+        ]
+
+        if duplicates:
+            raise ValueError(
+                "Duplicate future covariates names found in schema: " f"`{', '.join(duplicates)}`"
             )
 
         return v
